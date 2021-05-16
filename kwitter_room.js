f3a1f73a45d2074eaec5afd75ec82489c7b7e0fe
werty-lang <84279588+werty-lang@users.noreply.github.com>
2021-05-16 20:05:00 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAIQR2ljK1l-qfFqxMyGxPrdDSidftSnlE",
+    authDomain: "kwitter-home-work.firebaseapp.com",
+    databaseURL: "https://kwitter-home-work-default-rtdb.firebaseio.com",
+    projectId: "kwitter-home-work",
+    storageBucket: "kwitter-home-work.appspot.com",
+    messagingSenderId: "807655729057",
+    appId: "1:807655729057:web:c243252d1e2ca9fafc4de0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
